cbb20641ffc200a9cf14c6a4f630c64d4b45df5e
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2012-01-02 14:27:42 +0200

    Finale stage

---
+++ src/com/java/server/controllers/ChatUserController.java
@@ -0,0 +1,13 @@
+package com.java.server.controllers;
+
+public class ChatUserController {
+	
+	//HTTP GET
+	public String getAllChatMembers(){
+		
+	}
+	//HTTP GET
+	public String getMemberByRole(String role){
+		
+	}
+}
